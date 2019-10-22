af39ea7ea945d18eda5ca09c36e7985364da2f6f
armandof79 <a.ferreiras@gmail.com>
2019-10-22 11:02:51 -0400

    Initial commit

---
+++ js/basic-javascript-1.js
@@ -0,0 +1,5 @@
+// JavaScript source code
+var string = "this is a test string", string2 = "this is another";
+document.write(string);
+var string2 = string2.fontcolor("red")
+document.write(string2);
