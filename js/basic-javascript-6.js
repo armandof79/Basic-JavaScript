3c9b83c9347baf13c9fbe97f11f6f9c0b1f91f1d
armandof79 <a.ferreiras@gmail.com>
2019-10-25 12:06:38 -0400

    Create basic-javascript-6.js

---
+++ js/basic-javascript-6.js
@@ -0,0 +1,21 @@
+// JavaScript source code
+function Vehicle(Make, Model, Year, Color) {
+    this.Vehicle_Make = Make;
+    this.Vehicle_Model = Model;
+    this.Vehicle_Year = Year;
+    this.Vehicle_Color = Color;
+}
+var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
+var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "Red and Black");
+var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
+function My_Function() {
+    document.getElementById("New_and_This").innerHTML =
+        "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model +
+        "manufactured in " + Erik.Vehicle_Year;
+}
+function Parent_Function() {
+    document.getElementById("Nested_Function").innerHTML = Nested();
+        function Nested() {
+            Date();
+        }
+}
