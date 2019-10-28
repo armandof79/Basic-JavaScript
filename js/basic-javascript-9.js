5989558c5e733892e165faf362820b3f2b825461
armandof79 <a.ferreiras@gmail.com>
2019-10-28 16:06:08 -0400

    Update basic-javascript-9.js

--- js/basic-javascript-9.js
+++ js/basic-javascript-9.js
@@ -1,13 +1,42 @@
-// JavaScript source code
-function countdown() {
-    var seconds = document.getElementById("seconds").value;
-    function tick() {
-        seconds = seconds - 1;
-        timer.innerHTML = seconds;
-        setTimeout(tick, 1000);
-        if (seconds == -1) {
-            alert("Time's up!");
-        }
+//// JavaScript source code
+//function countdown() {
+//    var seconds = document.getElementById("seconds").value;
+//    function tick() {
+//        seconds = seconds - 1;
+//        timer.innerHTML = seconds;
+//        setTimeout(tick, 1000);
+//        if (seconds == -1) {
+//            alert("Time's up!");
+//        }
+//    }
+//    tick();
+//}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slide");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
-    tick();
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
 }
